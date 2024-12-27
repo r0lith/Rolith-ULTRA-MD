25f6f206f200d79befbdef4a464d6b6777b78c3b
r0lith <96643526+r0lith@users.noreply.github.com>
2024-12-28 01:38:10 +0530

    Update tools-fetch.js

--- plugins/tools-fetch.js
+++ plugins/tools-fetch.js
@@ -1,27 +1,46 @@
+import fetch from 'node-fetch';
+import { format } from 'util';
+import https from 'https';  // Import the https module
 
-import fetch from 'node-fetch'
-import { format } from 'util'
 let handler = async (m, { text, conn }) => {
-    if (!/^https?:\/\//.test(text)) throw `✳️ ${mssg.noLink('http:// o https://')}`
-    let _url = new URL(text)
-    let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY')
-    let res = await fetch(url)
+    if (!/^https?:\/\//.test(text)) throw `✳️ ${mssg.noLink('http:// o https://')}`;
+
+    let _url = new URL(text);
+    let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY');
+
+    // Create an HTTPS agent to bypass certificate verification
+    const agent = new https.Agent({ rejectUnauthorized: false });
+
+    // Pass the agent in the fetch options
+    let res = await fetch(url, { agent });
+
+    // Check content length
     if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
         // delete res
-        throw `Content-Length: ${res.headers.get('content-length')}`
+        throw `Content-Length: ${res.headers.get('content-length')}`;
     }
-    if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
-    let txt = await res.buffer()
-    try {
-        txt = format(JSON.parse(txt + ''))
-    } catch (e) {
-        txt = txt + ''
-    } finally {
-        m.reply(txt.slice(0, 65536) + '')
-    }
-}
-handler.help = ['get']
-handler.tags = ['tools']
-handler.command = /^(fetch|gets)$/i
 
-export default handler
+    // Check if the response is text or JSON
+    if (!/text|json/.test(res.headers.get('content-type'))) {
+        return conn.sendFile(m.chat, url, 'file', text, m);
+    }
+
+    // Fetch the response as a buffer
+    let txt = await res.buffer();
+
+    try {
+        // Attempt to parse the response as JSON
+        txt = format(JSON.parse(txt + ''));
+    } catch (e) {
+        txt = txt + '';
+    } finally {
+        // Send the response back, limiting the output length
+        m.reply(txt.slice(0, 65536) + '');
+    }
+};
+
+handler.help = ['get'];
+handler.tags = ['tools'];
+handler.command = /^(fetch|gets)$/i;
+
+export default handler;
