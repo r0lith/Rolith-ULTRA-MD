e67a3da8d5d88ab55af8124be24b8cc21edd1e82
r0lith <96643526+r0lith@users.noreply.github.com>
2025-01-02 10:54:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '919737825303;
+const defaultOwner = '919737825303';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
